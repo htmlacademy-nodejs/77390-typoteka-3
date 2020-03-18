6e6b7151679f828ee7d6fdbba6cf5927145d8db5
A.Alekseev <angelpsymail@gmail.com>
2020-03-18 21:01:38 +0500

    add cli service and help for it

---
+++ src/service/cli/help.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const {log} = require(`../../utils/log`);
+
+const HELP_TEXT = `Программа запускает http-сервер и формирует файл с данными для API.
+
+    Гайд:
+    server <command>
+
+    Команды:
+    --version:            выводит номер версии
+    --help:               печатает этот текст
+    --generate <count>    формирует файл mocks.json`;
+
+module.exports = {
+  name: `--help`,
+  run() {
+    log(HELP_TEXT, {status: `info`});
+  }
+};
