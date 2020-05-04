221520d04fa65c058dadcd45c146f5941e1ca130
A.Alekseev <angelpsymail@gmail.com>
2020-05-04 13:17:08 +0500

    fix errors read file

--- src/service/controls/api/helpers/articles.js
+++ src/service/controls/api/helpers/articles.js
@@ -5,8 +5,12 @@ require(`../../../../utils/env`);
 const FILE_NAME = process.env.MOCK_DATA_FILE_NAME;
 
 const getAllArticles = () => {
-  const fileContent = fs.readFileSync(FILE_NAME);
-  return JSON.parse(fileContent);
+  try {
+    const fileContent = fs.readFileSync(FILE_NAME);
+    return JSON.parse(fileContent);
+  } catch (e) {
+    return [];
+  }
 };
 
 const articles = getAllArticles();
